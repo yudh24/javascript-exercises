06d62ad5e8c285071539fddc31735746512a46c7
Panji Yudha <yudha.mawon24@gmail.com>
2022-06-21 22:12:12 +0700

    Submit leapYear, tempConversion, calculator

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temperature) {
+  let cels = (temperature - 32) * 5/9;
+  return Math.round(cels * 10)/10;
 };
 
-const ctof = function() {
-
+const ctof = function(temperature) {
+  let fahren = temperature * 9/5 + 32;
+  return Math.round(fahren * 10)/10;
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,45 @@
 const add = function() {
-	
+	return arguments[0]+arguments[1];
 };
 
 const subtract = function() {
-	
+	return arguments[0]-arguments[1];
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sumResult = 0;
+	arr.forEach(element => {
+    sumResult = sumResult + element;
+  });
+  return sumResult;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let multiplyResult = 1;
+	arr.forEach(element => {
+    multiplyResult = multiplyResult * element;
+  });
+  return multiplyResult;
 };
 
 const power = function() {
-	
+	return arguments[0] ** arguments[1];
 };
 
 const factorial = function() {
+  let factorialResult = 1;
+  if (arguments[0] == 0 || arguments[0] == 1){
+    return 1;
+  } else {
+    for (let i = arguments[0]; i > 0; i--){
+      factorialResult = factorialResult * i;
+    }
+    return factorialResult;
+  }
 	
 };
+
+console.log(factorial(2));
 
 // Do not edit below this line
 module.exports = {
